b8f162c85590f3c5aa2b1468964671fafba19020
Lucas <lucasbourdieu@gmail.com>
2015-03-22 22:33:44 -0300

    algo de codigo

--- assets/www/js/controllers/app.js
+++ assets/www/js/controllers/app.js
@@ -2,3 +2,61 @@ var phonegapnfc = angular.module("phonegapnfc", []);
 phonegapnfc.controller("HomeController", function ($scope) {
     $scope.message = "AngularJS!";
 });
+
+var app = {
+    initialize: function () {
+        this.bind();
+    },
+    bind: function () {
+        document.addEventListener('deviceready', this.deviceready, false);
+    },
+    deviceready: function () {
+    
+        function failure(reason) {
+            navigator.notification.alert(reason, function() {}, "There was a problem");
+        }
+
+        nfc.addNdefListener(
+            app.onNdef,
+            function() {
+                console.log("Listening for NDEF tags.");
+            },
+            failure
+        );
+
+        if (device.platform == "Android") {
+
+            // Android reads non-NDEF tag. BlackBerry and Windows don't.
+            nfc.addTagDiscoveredListener(
+                app.onNfc,
+                function() {
+                    console.log("Listening for non-NDEF tags.");
+                },
+                failure
+            );
+
+            // Android launches the app when tags with mime type text/pg are scanned
+            // because of an intent in AndroidManifest.xml.
+            // phonegap-nfc fires an ndef-mime event (as opposed to an ndef event)
+            // the code reuses the same onNfc handler
+            nfc.addMimeTypeListener(
+                'text/pg',
+                app.onNdef,
+                function() {
+                    console.log("Listening for NDEF mime tags with type text/pg.");
+                },
+                failure
+            );
+        }
+    },
+    onNfc: function (nfcEvent) {
+        $scope.message = nfcEvent.tag;     
+    },
+    onNdef: function (nfcEvent) {
+        $scope.message = nfcEvent.tag;       
+    },
+    clearScreen: function () {        
+        tagContents.innerHTML = "";
+        
+    }
+};
